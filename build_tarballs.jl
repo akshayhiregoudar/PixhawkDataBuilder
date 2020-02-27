5aecfa2d943eb1f2af06c8d520a47d49ed6e612e
Akshay H <akshayhiregoudar@tamu.edu>
2020-02-27 15:29:49 -0600

    update

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -5,14 +5,14 @@ name = "PixhawkData"
 version = v"1.0"
 # Collection of sources required to build PixhawkData
 sources = [
-    "https://github.com/akshayhiregoudar/PixhawkData/archive/master.tar.gz" =>
-    "a980534c70c6f86a69a65cae00a43c2194ea5a14b1eba02d19c908fb7d7f9114"
+    "https://github.com/akshayhiregoudar/PixhawkData/archive/v1.0.tar.gz" =>
+    "42b306b99b3ab14bcb1ca34efb63c55e7a80e59a3b8e3cb70a9a7cb844944f7d"
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/
-cd PixhawkData-master/
+cd PixhawkData-1.0
 
 make -j${nproc}
 
