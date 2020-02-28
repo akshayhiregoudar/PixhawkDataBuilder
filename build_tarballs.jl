7d0ebacf9ab7a10b9a72923c4dd23a9ad85b149e
Akshay H <akshayhiregoudar@tamu.edu>
2020-02-28 14:50:57 -0600

    a

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -14,12 +14,34 @@ script = raw"""
 cd $WORKSPACE/srcdir/
 cd PixhawkData-1.0.2
 
-make
+if [[ "${target}" == *-mingw* ]]; then
+    sed -i 's/cross_compiling=no/cross_compiling=yes/' configure
+    ./configure --prefix=$prefix --target=${target} --build=x86_64-w64-mingw32 --enable-silent-rules --enable-shared --enable-obj
+elif [[ "${target}" == *-apple-* ]]; then
+    sed -i 's/cross_compiling=no/cross_compiling=yes/' configure
+    export CC=/opt/${target}/bin/${target}-gcc
+    export CXX=/opt/${target}/bin/${target}-g++
+    ./configure --prefix=$prefix --target=${target} --enable-silent-rules --enable-shared --enable-obj
+else
+    autoreconf -fi
+    sed -i 's/cross_compiling=no/cross_compiling=yes/' configure
+    export CC=/opt/${target}/bin/${target}-gcc
+    export CXX=/opt/${target}/bin/${target}-g++
+    export LD=/opt/${target}/bin/${target}-ld
+    export LDFLAGS=-L/opt/${target}/${target}/lib64
+    ./configure --prefix=$prefix --target=${target} --enable-silent-rules --enable-shared --enable-obj
+fi
+make -j${nproc} check
+make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = [
+    Linux(:x86_64, libc=:glibc),
+    MacOS(:x86_64),
+    Windows(:x86_64)
+]
 
 # The products that we will ensure are always built
 products(prefix) = [
